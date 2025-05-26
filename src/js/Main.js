4b8b8bf0b3c4cfa29dc5c0621c74592918f35a80
petriwna <petriwna43@gmail.com>
2025-05-26 23:46:48 +0300

    init

---
+++ src/js/Main.js
@@ -0,0 +1,34 @@
+export class Main {
+    constructor() {
+        this.mobileMenu = document.querySelector('.mobile-menu');
+        this.menuBtnOpen = document.querySelector('.menu-btn-open');
+        this.menuBtnClose = document.querySelector('.menu-btn-close');
+
+        this.addEventListeners();
+    }
+
+    addEventListeners() {
+        this.menuBtnOpen.addEventListener('click', this.handleMenuButton.bind(this));
+        this.menuBtnClose.addEventListener('click', this.handleMenuButton.bind(this));
+    }
+
+
+    handleMenuButton() {
+        this.toggleMenu();
+        this.disableScroll();
+    }
+
+    toggleMenu() {
+        this.mobileMenu.classList.toggle('is-open');
+    }
+
+    disableScroll() {
+        document.body.classList.toggle('is-scroll-disable');
+    }
+
+    handleFormSubmit(event) {
+        event.preventDefault();
+
+        window.location.href = 'thank-you.html';
+    }
+}
